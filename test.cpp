b089b4ed0ca2624898fbfb4351ad68e2666c578d
stevegordon <tdtsgordon@gmail.com>
2018-09-06 01:17:25 -0700

    initialize c++

---
+++ test.cpp
@@ -0,0 +1,20 @@
+//
+//  test.cpp
+//  
+//
+//  Created by Steven Gordon on 9/5/18.
+//
+
+#include "test.hpp"
+
+#include <iostream>
+using std::cout;
+
+int main()
+{
+    std::cout << "Hello World!\n";
+    return 0;
+}
+
+
+
---
+++ test.hpp
@@ -0,0 +1,13 @@
+//
+//  test.hpp
+//  
+//
+//  Created by Steven Gordon on 9/5/18.
+//
+
+#ifndef test_hpp
+#define test_hpp
+
+#include <stdio.h>
+
+#endif /* test_hpp */
